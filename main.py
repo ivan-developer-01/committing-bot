636502c25bb9c90fe58f900f1ebb646367de3ba3
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-08 09:11:34 +0300

    Create logic in main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+
+commit_count = -1
+
+with open('commit_count.txt', 'r') as f:
+	commit_count = int(f.read())
+
+if commit_count == -1:
+	raise ValueError("Check the commit_count.txt")
+
+for i in range(commit_count):
+	os.system(f'echo "Commit {str(i + 1)}" > main.txt')
+	os.system(f'git add .')
+	os.system(f'git commit -m "Auto-commit {str(i + 1)} at {time.strftime("%Y-%m-%d %H:%M:%S")}"')
+
+commit_count += 1
+
+with open('commit_count.txt', 'w') as f:
+	f.write(str(commit_count))
+
+print("executing: git push")
