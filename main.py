0e50112ca09da43502913ce26055105d2ffb28f9
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-08 09:19:09 +0300

    update logic in main.py

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ for i in range(commit_count):
 	os.system(f'echo "Commit {str(i + 1)}" > main.txt')
 	os.system(f'git add .')
 	os.system(f'git commit -m "Auto-commit {str(i + 1)} at {time.strftime("%Y-%m-%d %H:%M:%S")}"')
+	time.sleep(0.3)
 
 commit_count += 1
 
 with open('commit_count.txt', 'w') as f:
 	f.write(str(commit_count))
+	os.system(f'git add .')
+	os.system(f'git commit -m "Auto-commit to update commit_count.txt at {time.strftime("%Y-%m-%d %H:%M:%S")}"')
+	time.sleep(0.5)
 
 os.system("git push")
